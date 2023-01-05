b4409b1a8fd008b1447048d59820cd4c38e086be
unknown <admin@skm.vn>
2023-01-05 17:40:57 +0700

    Update

---
+++ version.js
@@ -0,0 +1,9 @@
+version = "2";
+libs = [
+  "./jsdelivr/js/phaser-ads.min.js",
+  "./jsdelivr/js/phaser-cachebuster.min.js",
+  "./jsdelivr/js/phaser-nineslice.min.js",
+  "./jsdelivr/js/phaser-spine.min.js",
+  "./jsdelivr/js/phaser-super-storage.min.js",
+  "./jsdelivr/js/splash.min.js"
+];
